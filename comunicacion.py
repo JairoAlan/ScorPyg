ad552d37932f4d8dd3aa0799d9405a0c513cd3bf
Jairo Alan Castelan Fernandez <jairoalanc@outlook.com>
2024-05-15 15:03:54 -0600

    Acomodando ya los datos, convertidos a lista y se accede a los datos....

--- comunicacion.py
+++ comunicacion.py
@@ -9,17 +9,36 @@ import csv
 df = pd.read_csv('data_Sat.csv')
 
 async def main():
-    reader, writer = await serial_asyncio.open_serial_connection(url='COM3', baudrate=115200)
-
-    
+    reader, writer = await serial_asyncio.open_serial_connection(url='COM3', baudrate=115200)    
     writer.write(("AT+PARAMETER=7,7,1,4\r\n").encode())
     await writer.drain()
 
     while True:
-        data = await reader.read(100)
+        data = await reader.read(400)
+        await asyncio.sleep(1)
         if data:
-            print(data.decode())
+            #print(data.decode())
+            if len(data) > 140 and data.decode().startswith("+RCV=1"):
+                datos = data.decode()
+                lista = datos.split(',')
+                temp = lista[2]
+                preHp = lista[3]
+                altitud = lista[4]
+                acc_x = lista[5]
+                acc_y = lista[6]
+                acc_z = lista[7]
+                gy_x = lista[8]
+                gy_y = lista[9]
+                gy_z = lista[10]
+                velocidad = lista[11]
+                aceleracion = lista[12]
+                latitud = lista[13]
+                longitud = lista[14]
+                altGps = lista[15]
+                tiempo = lista[16]
+                
+                
+                print(temp,preHp,altitud,acc_x,acc_y,acc_z,gy_x,gy_y,gy_z,velocidad,aceleracion,latitud,longitud,altGps,tiempo)
 
 asyncio.run(main())
-
 
